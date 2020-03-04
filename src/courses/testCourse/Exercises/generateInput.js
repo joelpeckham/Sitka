17b5d78ea16ef643e8ce273e8e27b6209aabf3c9
joelpeckham <joelskyler@gmail.com>
2020-03-04 14:02:47 -0500

    Updated generate input test exercise

--- src/courses/testCourse/Exercises/generateInput.js
+++ src/courses/testCourse/Exercises/generateInput.js
@@ -5,12 +5,12 @@ module.exports = function generateInput(config = null) {
   let randNum = helpers.randomInt(-10000,10001);
 
   return {
-    question: `Assign the variable x with user input.`,
-    pre: [`pyInput = input`,`def input(prompt = ''):\n    pyInput(prompt)\n    return (${randNum})\n`], // set things up--variables, utility functions, etc.
+    question: `Write a single expression that casts the user's input as an int and assigns the value to x.`,
+    pre: [`pyInput = input`,`def input(prompt = ''):\n    pyInput(prompt)\n    return ('${randNum}')\n`], // set things up--variables, utility functions, etc.
     post: ['x'], // output whatever you asked the student to set up so we can check it
     check: function(response){
       return {
-        result: (response.teardown.x == randNum),
+        result: (response.teardown.x == `${randNum}`),
         correctMessage: helpers.randomCorrectMessage(),
         wrongMessage: helpers.randomIncorrectMessage()
       }
--- src/js/TermData.js
+++ src/js/TermData.js
@@ -5,6 +5,7 @@ module.exports = class TermData {
     this.blockData = false
 
     this.term.onData(data => {
+      console.dir({data:data})
       if (this.isGoodData(data)) {this.callbackFunction(data)}
     })
   }
@@ -16,6 +17,6 @@ module.exports = class TermData {
   isGoodData(data){
     let badStrings = ["[A","[B", "", "", ""]
     if (this.blockData && badStrings.indexOf(data) >= 0) {return false}
-    else{return true}
+    return true
   }
 }
